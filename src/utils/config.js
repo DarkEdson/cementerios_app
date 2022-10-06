d5be80e803fbf61ade366d2bdd52e039237aa35a
Edson Aju <avataredson@gmail.com>
2022-10-06 15:58:17 -0600

    ip

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'http://34.125.91.120:4000/api';
+//34.125.91.120
 export const BASE_URL_IMG = 'http://34.125.91.120/images/';
 export const PRODUCTS_URL = 'products/';
 export const CATEGORIES_URL = 'categories/';
