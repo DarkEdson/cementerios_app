18fcbb3283f5af197ccb6a0d63958d3a9287f7ae
Edson Aju <avataredson@gmail.com>
2022-10-06 15:00:25 -0600

    [FIX] product by cat

--- src/Apis/ProductsApi.js
+++ src/Apis/ProductsApi.js
@@ -1,157 +1,128 @@
-import { BASE_URL, BASE_URL_IMG, PRODUCTS_URL } from '@utils/config';
+import {BASE_URL, BASE_URL_IMG, PRODUCTS_URL} from '@utils/config';
 
 async function productbyCountry(country, lenguaje) {
-    let url = `${BASE_URL}/product.getprodsbycou/${country.value}/${lenguaje._id}`;
-    let productos = [];
-    try {
-        await fetch(url, {
-            method: 'GET',
-            redirect: 'follow',
-        })
-            .then(res => res.json())
-            .catch(error => console.error('Error', error))
-            .then(response => {
-                response.forEach(producto => {
-                    let multimediaP = []
-                    if(producto.hasOwnProperty('multimedia')){
-
-                    }
-                    producto.labels.forEach((multim, key) => {
-                        let imagen={}
-                        let nombrePerzonalizado = 'imagen'+key;
-                        let valorPerzonalizado = `${BASE_URL_IMG}${PRODUCTS_URL}${multim.price}`;
-                        imagen[nombrePerzonalizado] = valorPerzonalizado;
-                        console.log(imagen,'TEST MULTIMEDIA')
-                        multimediaP.push(imagen)
-                    })
-                    productos.push({
-                        _id: producto._id,
-                        idCategory: producto.idCategory,
-                        idHeadquarter: producto.idHeadquarter,
-                        code: producto.code,
-                        principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${producto.image}`,
-                        name: producto.labels[0].name,
-                        description: producto.labels[0].description,
-                        price: producto.labels[0].price,
-                        multimedia: multimediaP
-                    });
-                });
-            });
-        return productos;
-    } catch (error) {
-        console.error(error);
-        return productos;
-    }
+  let url = `${BASE_URL}/product.getprodsbycou/${country.value}/${lenguaje._id}`;
+  let productos = [];
+  try {
+    await fetch(url, {
+      method: 'GET',
+      redirect: 'follow',
+    })
+      .then(res => res.json())
+      .catch(error => console.error('Error', error))
+      .then(response => {
+        response.forEach(producto => {
+          productos.push({
+            _id: producto._id,
+            idCategory: producto.idCategory,
+            idHeadquarter: producto.idHeadquarter,
+            code: producto.code,
+            principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${producto.image}`,
+            name: producto.labels[0].name,
+            description: producto.labels[0].description,
+            price: producto.labels[0].price,
+          });
+        });
+      });
+    return productos;
+  } catch (error) {
+    console.error(error);
+    return productos;
+  }
 }
 
 async function productFullbyCategory(product, lenguaje) {
-    let url = `${BASE_URL}/product.getprdsbycat/${product._id}/${lenguaje._id}`;
-    let productos = [];
-    try {
-        await fetch(url, {
-            method: 'GET',
-            redirect: 'follow',
-        })
-            .then(res => res.json())
-            .catch(error => console.error('Error', error))
-            .then(response => {
-                response.forEach(producto => {
-                    let multimediaP = []
-                    if(producto.hasOwnProperty('multimedia')){
-
-                    }
-                    producto.labels.forEach((multim, key) => {
-                        let nombrePerzonalizado = 'imagen'+key;
-                        let valorPerzonalizado = `${BASE_URL_IMG}${PRODUCTS_URL}${multim.price}`;
-                        imagen[nombrePerzonalizado] = valorPerzonalizado;
-                        console.log(imagen,'TEST MULTIMEDIA')
-                        multimediaP.push(imagen)
-                    })
-                    productos.push({
-                        _id: producto._id,
-                        idCategory: producto.idCategory,
-                        idHeadquarter: producto.idHeadquarter,
-                        code: producto.code,
-                        principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${producto.image}`,
-                        name: producto.labels[0].name,
-                        description: producto.labels[0].description,
-                        price: producto.labels[0].price,
-                        multimedia: multimediaP
-                    });
-                });
-            });
-        return productos;
-    } catch (error) {
-        console.error(error);
-        return productos;
-    }
+  console.log(product, 'FULL PROD PRODUCT');
+  let url = `${BASE_URL}/product.findbyidandlan/${product._id}/${lenguaje._id}`;
+  let productos = {};
+  try {
+    await fetch(url, {
+      method: 'GET',
+      redirect: 'follow',
+    })
+      .then(res => res.json())
+      .catch(error => console.error('Error FULL PROD', error))
+      .then(response => {
+        console.log(response, 'FULL PROD RESPONSE');
+        productos = {
+          _id: response._id,
+          idCategory: response.idCategory,
+          idHeadquarter: response.idHeadquarter,
+          code: response.code,
+          principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${response.image}`,
+          name: response.labels[0].name,
+          description: response.labels[0].description,
+          price: response.labels[0].price,
+        };
+      });
+    return productos;
+  } catch (error) {
+    console.error(error, 'FULL PROD RETURN');
+    return productos;
+  }
 }
 
 async function productbyCategory(Category) {
-    let url = `${BASE_URL}/product.getprdsbycat/${Category._id}`;
+  let url = `${BASE_URL}/product.getprdsbycat/${Category._id}`;
 
-    let productos = [];
-    try {
-        await fetch(url, {
-            method: 'GET',
-            redirect: 'follow',
-        })
-            .then(res => res.json())
-            .catch(error => console.error('Error', error))
-            .then(response => {
-                response.forEach(async producto => {
-
-                    productos.push(producto)
-                });
-            });
-        return productos;
-    } catch (error) {
-        console.error(error);
-        return productos;
-    }
+  let productos = [];
+  try {
+    await fetch(url, {
+      method: 'GET',
+      redirect: 'follow',
+    })
+      .then(res => res.json())
+      .catch(error => console.error('Error byCAT', error))
+      .then(response => {
+        response.forEach(async producto => {
+          productos.push({
+            _id: producto._id,
+            code: producto.code,
+            principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${producto.image}`,
+          });
+        });
+      });
+    return productos;
+  } catch (error) {
+    console.error('RESPONSE ERROR BY CAT', error);
+    return productos;
+  }
 }
 
 async function productbyHeadquarters(Sede, lenguaje) {
-    let url = `${BASE_URL}/product.getprdsbyhq/${Sede._id}/${lenguaje._id}`;
-    let productos = [];
-    try {
-        await fetch(url, {
-            method: 'GET',
-            redirect: 'follow',
-        })
-            .then(res => res.json())
-            .catch(error => console.error('Error', error))
-            .then(response => {
-                response.forEach(producto => {
-                    let multimediaP = []
-                    if(producto.hasOwnProperty('multimedia')){
-
-                    }
-                    producto.labels.forEach((multim, key) => {
-                        let nombrePerzonalizado = 'imagen'+key;
-                        let valorPerzonalizado = `${BASE_URL_IMG}${PRODUCTS_URL}${multim.price}`;
-                        imagen[nombrePerzonalizado] = valorPerzonalizado;
-                        console.log(imagen,'TEST MULTIMEDIA')
-                        multimediaP.push(imagen)
-                    })
-                    productos.push({
-                        _id: producto._id,
-                        idCategory: producto.idCategory,
-                        idHeadquarter: producto.idHeadquarter,
-                        code: producto.code,
-                        principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${producto.image}`,
-                        name: producto.labels[0].name,
-                        description: producto.labels[0].description,
-                        price: producto.labels[0].price,
-                        multimedia: multimediaP
-                    });
-                });
-            });
-        return productos;
-    } catch (error) {
-        console.error(error);
-        return productos;
-    }
+  let url = `${BASE_URL}/product.getprdsbyhq/${Sede._id}/${lenguaje._id}`;
+  let productos = [];
+  try {
+    await fetch(url, {
+      method: 'GET',
+      redirect: 'follow',
+    })
+      .then(res => res.json())
+      .catch(error => console.error('Error', error))
+      .then(response => {
+        response.forEach(producto => {
+          productos.push({
+            _id: producto._id,
+            idCategory: producto.idCategory,
+            idHeadquarter: producto.idHeadquarter,
+            code: producto.code,
+            principalImage: `${BASE_URL_IMG}${PRODUCTS_URL}${producto.image}`,
+            name: producto.labels[0].name,
+            description: producto.labels[0].description,
+            price: producto.labels[0].price,
+          });
+        });
+      });
+    return productos;
+  } catch (error) {
+    console.error(error);
+    return productos;
+  }
 }
 
-export { productbyCountry, productbyCategory, productbyHeadquarters, productFullbyCategory };
+export {
+  productbyCountry,
+  productbyCategory,
+  productbyHeadquarters,
+  productFullbyCategory,
+};
--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,78 +1,81 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 //Apis
-import { productbyCountry, productbyCategory, productbyHeadquarters ,productFullbyCategory} from '@Apis/ProductsApi';
+import {
+  productbyCountry,
+  productbyCategory,
+  productbyHeadquarters,
+  productFullbyCategory,
+} from '@Apis/ProductsApi';
 
 export const ProductsContext = createContext();
 
-export const ProductsProvider = ({ children }) => {
-    const [ProductsCountry, setProductsCountry] = useState([]);
-    const [ProductsCategory, setProductsCategory] = useState([]);
-    const [ProductsFullCategory, setProductsFullCategory] = useState([]);
-    const [ProductsSedes, setProductsSede] = useState([]);
-    const [isLoadingProducts, setisLoadingProducts] = useState(true)
+export const ProductsProvider = ({children}) => {
+  const [ProductsCountry, setProductsCountry] = useState([]);
+  const [ProductsCategory, setProductsCategory] = useState([]);
+  const [ProductsFullCategory, setProductsFullCategory] = useState([]);
+  const [ProductsSedes, setProductsSede] = useState([]);
+  const [isLoadingProducts, setisLoadingProducts] = useState(true);
 
+  const getProductsbyCountry = async (country, languaje) => {
+    setisLoadingProducts(true);
+    productbyCountry(country, languaje).then(res => {
+      res.sort((a, b) => a.code.localeCompare(b.code));
+      console.log('PRODUCTOS por PAIS', res);
+      setProductsCountry(res);
+      setisLoadingProducts(false);
+    });
+  };
 
+  const getProductsbyCategory = async category => {
+    setisLoadingProducts(true);
+    productbyCategory(category).then(res => {
+      res.sort((a, b) => a.code.localeCompare(b.code));
+      console.log('PRODUCTOS por CATEGORIA', res);
+      setProductsCategory(res);
+      setisLoadingProducts(false);
+    });
+  };
 
-    const getProductsbyCountry = async (country, languaje) => {
-        setisLoadingProducts(true)
-        productbyCountry(country, languaje).then(res => {
-            res.sort((a, b) => a.code.localeCompare(b.code));
-            console.log('PRODUCTOS por PAIS', res)
-            setProductsCountry(res)
-            setisLoadingProducts(false);
-        });
+  const getProductsFullbyCategory = async (productos, languaje) => {
+    setisLoadingProducts(true);
+    let productsFULL = [];
+    productos.forEach(producto => {
+      productFullbyCategory(producto, languaje).then(res => {
+        console.log(res);
+        productsFULL.push(res);
+      });
+    });
+    console.log('PRODUCTOS COMPLETOS por CATEGORIA', productsFULL);
+    setProductsFullCategory(productsFULL);
+    setisLoadingProducts(false);
+  };
 
-    };
+  const getProductsbySede = async (sede, languaje) => {
+    setisLoadingProducts(true);
+    productbyHeadquarters(sede, languaje).then(res => {
+      res.sort((a, b) => a.code.localeCompare(b.code));
+      console.log('PRODUCTOSpor SEDE', res);
+      setProductsSede(res);
+      setisLoadingProducts(false);
+    });
+  };
 
-    const getProductsbyCategory = async (category) => {
-        setisLoadingProducts(true)
-        productbyCategory(category).then(res => {
-            res.sort((a, b) => a.code.localeCompare(b.code));
-            console.log('PRODUCTOS por CATEGORIA', res)
-            setProductsCategory(res)
-            setisLoadingProducts(false);
-        });
+  useEffect(() => {}, []);
 
-    };
-
-    const getProductsFullbyCategory = async (producto, languaje) => {
-        setisLoadingProducts(true)
-        productFullbyCategory (producto, languaje).then(res => {
-            res.sort((a, b) => a.code.localeCompare(b.code));
-            console.log('PRODUCTOS COMPLETOS por CATEGORIA', res)
-            setProductsFullCategory(res)
-            setisLoadingProducts(false);
-        });
-
-    };
-
-    const getProductsbySede = async (sede, languaje) => {
-        setisLoadingProducts(true)
-        productbyHeadquarters(sede, languaje).then(res => {
-            res.sort((a, b) => a.code.localeCompare(b.code));
-            console.log('PRODUCTOSpor SEDE', res)
-            setProductsSede(res)
-            setisLoadingProducts(false);
-        });
-
-    };
-
-    useEffect(() => { }, []);
-
-    return (
-        <ProductsContext.Provider
-            value={{
-                ProductsCountry,
-                ProductsCategory,
-                ProductsFullCategory,
-                ProductsSedes,
-                isLoadingProducts,
-                getProductsbyCountry,
-                getProductsbyCategory,
-                getProductsbySede,
-                getProductsFullbyCategory,
-            }}>
-            {children}
-        </ProductsContext.Provider>
-    );
+  return (
+    <ProductsContext.Provider
+      value={{
+        ProductsCountry,
+        ProductsCategory,
+        ProductsFullCategory,
+        ProductsSedes,
+        isLoadingProducts,
+        getProductsbyCountry,
+        getProductsbyCategory,
+        getProductsbySede,
+        getProductsFullbyCategory,
+      }}>
+      {children}
+    </ProductsContext.Provider>
+  );
 };
