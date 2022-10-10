69569464bce7b6ed5aa11593160df470d72857d9
Edson Aju <avataredson@gmail.com>
2022-10-10 14:42:48 -0600

    [fix] carrito

--- src/context/ShoppingCartContext.js
+++ src/context/ShoppingCartContext.js
@@ -1,103 +1,99 @@
-import React, { createContext, useState, useEffect } from 'react';
-import { apiPago } from '@Apis/ApisGenerales';
+import React, {createContext, useState, useEffect} from 'react';
+import {apiPago} from '@Apis/ApisGenerales';
 import Snackbar from 'react-native-snackbar';
 
 const initialState = [];
 
 const ShoppingCartContext = createContext();
 
-function ShoppingCartProvider({ children }) {
+function ShoppingCartProvider({children}) {
   const [ShoppingCart, setShoppingCart] = useState(initialState);
-  const [afiliateCart, setafiliateCart] = useState({})
-  const [isLoadingCart, setisLoadingCart] = useState(false)
-  const [recipe, setrecipe] = useState({})
-  const [carrito, setcarrito] = useState(false)
-  const [rutaCart, setrutaCart] = useState(false)
-  let actualCart
-  let existe = false
+  const [afiliateCart, setafiliateCart] = useState({});
+  const [isLoadingCart, setisLoadingCart] = useState(false);
+  const [recipe, setrecipe] = useState({});
+  const [carrito, setcarrito] = useState(false);
+  const [rutaCart, setrutaCart] = useState(false);
+  let actualCart;
+  let existe = false;
 
   useEffect(() => {
-    existe = false
+    existe = false;
     if (ShoppingCart.length > 0) {
-      setcarrito(true)
+      setcarrito(true);
     } else {
-      setcarrito(false)
+      setcarrito(false);
     }
-
-  }, [])
+  }, []);
 
   function addItemtoCart(item) {
-    existe = false
-    let itemRepetido
+    existe = false;
+    let itemRepetido;
     actualCart = ShoppingCart;
     if (actualCart.length >= 1) {
       actualCart.map(prod => {
         if (prod._id === item._id) {
-          itemRepetido = prod
-          actualCart = actualCart.filter(item => item !== prod)
-          console.log('CARRO TRAS FILTRO', actualCart)
-          existe = true
+          itemRepetido = prod;
+          actualCart = actualCart.filter(item => item !== prod);
+          console.log('CARRO TRAS FILTRO', actualCart);
+          existe = true;
         } else {
           if (!existe) {
-            existe = false
+            existe = false;
           }
         }
-
-      })
+      });
       if (!existe) {
         actualCart.push(item);
       } else {
-        actualCart.push(
-          {
-            "_id": itemRepetido._id,
-            "cantidad": itemRepetido.cantidad + item.cantidad,
-            "code": itemRepetido.code,
-            "description": itemRepetido.description,
-            "idCategory": itemRepetido.idCategory,
-            "idHeadquarter": itemRepetido.idHeadquarter,
-            "name": itemRepetido.name,
-            "price": itemRepetido.price,
-            "principalImage": itemRepetido.principalImage
-          }
-        )
-        console.log('CARRITO TRAS PUSH', actualCart)
+        actualCart.push({
+          _id: itemRepetido._id,
+          cantidad: itemRepetido.cantidad + item.cantidad,
+          code: itemRepetido.code,
+          description: itemRepetido.description,
+          idCategory: itemRepetido.idCategory,
+          idHeadquarter: itemRepetido.idHeadquarter,
+          name: itemRepetido.name,
+          price: itemRepetido.price,
+          principalImage: itemRepetido.principalImage,
+        });
+        console.log('CARRITO TRAS PUSH', actualCart);
       }
-    }
-    else {
+    } else {
       actualCart.push(item);
     }
 
-    console.log('Carrito Actual', actualCart)
-    console.log('item a agregar', item)
-    setcarrito(true)
-    setShoppingCart(actualCart)
+    console.log('Carrito Actual', actualCart);
+    console.log('item a agregar', item);
+    setcarrito(true);
+    setShoppingCart(actualCart);
   }
 
   function removeItemtoCart(value) {
-    existe = false
+    existe = false;
     actualCart = ShoppingCart;
 
     if (actualCart.length >= 1) {
-      console.log('Carrito Actual', actualCart)
-      console.log('item a borrar', value)
-      actualCart = actualCart.filter(item => item !== value)
-      setShoppingCart(actualCart)
+      console.log('Carrito Actual', actualCart);
+      console.log('item a borrar', value);
+      actualCart = actualCart.filter(item => item !== value);
+      setShoppingCart(actualCart);
+      if (actualCart.length == 0) {
+        console.log('carrito vacio');
+        setcarrito(false);
+      }
     } else {
-      console.log('carrito vacio')
-      setcarrito(false)
+      console.log('carrito vacio');
+      setcarrito(false);
     }
-
-
   }
 
   function removeAllItemstoCart() {
-    existe = false
-    console.log('carrito vacio')
-    setShoppingCart([])
-    setShoppingCart([])
-    setcarrito(false)
+    existe = false;
+    console.log('carrito vacio');
+    setShoppingCart([]);
+    setShoppingCart([]);
+    setcarrito(false);
   }
-
 
   async function sendShoppingCartSell(dataCart, goToScreen, routeName) {
     setisLoadingCart(true);
@@ -109,24 +105,32 @@ function ShoppingCartProvider({ children }) {
         text: res.status,
         duration: Snackbar.LENGTH_LONG,
       });
-      console.log('carrito vacio')
-      setShoppingCart([])
-      setcarrito(false)
-      goToScreen(routeName)
+      console.log('carrito vacio');
+      setShoppingCart([]);
+      setcarrito(false);
+      goToScreen(routeName);
     });
   }
 
-
   return (
-    <ShoppingCartContext.Provider value={{
-      ShoppingCart, addItemtoCart, removeItemtoCart,
-      removeAllItemstoCart, afiliateCart, setafiliateCart,
-      carrito, rutaCart, setrutaCart, recipe, sendShoppingCartSell
-      , isLoadingCart
-    }}>
+    <ShoppingCartContext.Provider
+      value={{
+        ShoppingCart,
+        addItemtoCart,
+        removeItemtoCart,
+        removeAllItemstoCart,
+        afiliateCart,
+        setafiliateCart,
+        carrito,
+        rutaCart,
+        setrutaCart,
+        recipe,
+        sendShoppingCartSell,
+        isLoadingCart,
+      }}>
       {children}
     </ShoppingCartContext.Provider>
   );
 }
 
-export { ShoppingCartContext, ShoppingCartProvider };
+export {ShoppingCartContext, ShoppingCartProvider};
